552d743f2b40d52e93a46a18e8016a55958f46b2
Becca Henegar <rh4xfh@gmail.com>
2022-09-28 22:28:28 -0500

    updated restaurant studio

---
+++ src/com/company/MenuItem.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Currency;
+import java.util.Date;
+
+public class MenuItem {
+    private Date dateAdded;
+    private Boolean isNew;
+    private Currency price;
+    private String description;
+    private String name;
+    private Category category;
+}
